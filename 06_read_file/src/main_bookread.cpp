1852491fc915a86c1d3121e6e2ad5ffbd22260c1
Impulsul <andrei.parvan81@gmail.com>
2021-08-10 13:44:27 +0300

    resolving 06_read_file exercise

--- 06_read_file/src/main_bookread.cpp
+++ 06_read_file/src/main_bookread.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 /**
 	Define a simple book.
@@ -13,8 +16,13 @@ public:
 
 	void print()
 	{
-		std::cout << this->name << std::endl;
-		std::cout << "\t(by " << this->authors << ")" << std::endl;
+		std::cout << name << std::endl;
+		std::cout << "\t(by " << authors << ")" << std::endl;
+	}
+
+	Book(string Name, string Authors) {
+		this->name = Name;
+		this->authors = Authors;
 	}
 };
 
@@ -31,11 +39,44 @@ public:
 	@param file_name The name of the file to read from (must include path).
 	@return Vector of books.
 */
-std::vector<Book> readBooksFromTextFile(const std::string& file_name)
+std::vector<Book*> readBooksFromTextFile(const std::string& file_name)
 {
-	std::vector<Book> results;
+	string line;
+	vector<string> Name;
+	vector<string> authors;
+	vector<Book*> results;
+	int counter = 1;
 	// TODO: BEGIN read the file -------------------------------------
+	ifstream sourceFile(file_name);
+	if (sourceFile.is_open()) {
+		while (getline(sourceFile, line)) {
+			istringstream sir(line);
+				
+					string A;
+					string B;
+					sir >> A;
+					sir >> B;
+					if (counter % 2 != 0) {
+						Name.push_back( A + B);
+					}
+					else {
+						authors.push_back(A + B);
+					}
+				
 
+			
+			
+			counter++;
+		}
+		sourceFile.close();
+	}
+	else {
+		cout << "Invalid Connexion" << endl;
+	}
+	for (int i = 0; i < Name.size(); i++) {
+		Book* carte = new Book(Name[i], authors[i]);
+		results.push_back(carte);
+	}
 
 	// E.g. Book myBook;
 	//		...
@@ -52,15 +93,15 @@ int main()
 	// E.g. https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
 
 	// Read the data
-	std::string input_data("../../data/ermahgerd_berks.txt");
+	std::string input_data("C:/Users/yoyol/source/repos/Tema2/errmahgerd_berks.txt");
 	std::cout << "Reading the data from " << input_data << std::endl;
-	std::vector<Book> books_from_file = readBooksFromTextFile(input_data);
+	std::vector<Book*> books_from_file = readBooksFromTextFile(input_data);
+	
 
 	// Print the data
 	std::cout << "Here are all the books found in the data file..." << std::endl;
-	for (auto book : books_from_file)
-	{
-		book.print();
+	for (int i = 0; i < books_from_file.size(); i++) {
+		books_from_file[i]->print();
 	}
 
 	return 0;
