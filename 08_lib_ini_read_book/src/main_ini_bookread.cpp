52e951b0214b0200374560f8464035a548237db5
Impulsul <andrei.parvan81@gmail.com>
2021-08-16 19:00:03 +0300

    add resolve for 08_lib_ini_read_book and Game of Life

--- 08_lib_ini_read_book/src/main_ini_bookread.cpp
+++ 08_lib_ini_read_book/src/main_ini_bookread.cpp
@@ -8,59 +8,68 @@
 
 // 3rd party libs headers
 #include <SimpleIni.h>
-
-/**
-	Define a simple book.
-*/
+// am folosit fisiere header si cpp
+// aici este fisierul Book.h
 class Book
 {
-public:
+private:
 	std::string name;
 	std::string authors;
+public:
+	Book(std::string Name, std::string Authors);
 
-	void print()
-	{
-		std::cout << this->name << std::endl;
-		std::cout << "\t(by " << this->authors << ")" << std::endl;
-	}
+	//afisare carti
+	void print();
+
+	//citire fisier
+	std::vector<Book> static readBooksFromIniFile(const std::string& file_name);
+
 };
+// pana aici
+//apoi fisierul Book.cpp
+//#include"Book.h"
+//#include <iostream>
+Book::Book(std::string Name, std::string Authors) {
+	this->name = Name;
+	this->authors = Authors;
+}
 
-/**
-	Reads a vector of books from an INI file.
-	Note: the INI file shall contain a general books section with the number of books contained,
-	and another INI section for each book, named [book.N], where N is a number/counter
+void Book::print() {
+	std::cout << this->name << std::endl;
+	std::cout << "\t(by " << this->authors << ")" << std::endl;
+}
 
-	[books]
-	count = 2;
-
-	[book.1]
-	name=The origin of truth
-	author=Gusti
-	[book.2]
-	name  = Arhanghelul Raul
-	author=Ovidiu Eftimie
-
-	@param file_name The name of the file to read from (must include path).
-	@return Vector of books.
-*/
-std::vector<Book> readBooksFromIniFile(const std::string& file_name)
-{
+std::vector<Book> Book::readBooksFromIniFile(const std::string& file_name) {
 	std::vector<Book> results;
-	// TODO: BEGIN read the file -------------------------------------
+
+	CSimpleIniA ini;
+	ini.SetUnicode();
+
+	SI_Error rc = ini.LoadFile(file_name.c_str());
+	if (rc < 0) {
+		std::cout << "crapa" << std::endl;
+	}
+
+	CSimpleIniA::TNamesDepend sections;
+	ini.GetAllSections(sections);
 	
-	// E.g. Book myBook;
-	//		// build the section name (E.g. book.1)
-	//		std::stringstream ss;
-	//		ss << "book." << (i + 1);
-	//		// Copy the stream to a string you can use
-	//		std::string section_name(ss.str());
+	for (auto index : sections) {
+		CSimpleIniA::TNamesDepend keys;
+		ini.GetAllKeys(index.pItem, keys);
+		
+			const char* carte_name = ini.GetValue(index.pItem, "name");
 
-	//		...
-	//		results.emplace_back(myBook);
-
-	// TODO: END read file and add to results vector ------------------
+			const char* carte_author = ini.GetValue(index.pItem, "author");
+			if (carte_name == NULL) {
+				break;
+			}
+			Book carte(carte_name, carte_author);
+			results.emplace_back(carte);
+		
+	}
 	return results;
 }
+//pana aici ulterior fisierul main...
 
 int main()
 {
@@ -68,16 +77,14 @@ int main()
 	// Using the SimpleINI C++ Lib: https://github.com/brofield/simpleini
 
 	// Read the data
-	std::string input_data("PATH_TO_INI_FILE.ini");
+	const std::string input_data("ermahgerd_berks.ini");
 	std::cout << "Reading the data from " << input_data << std::endl;
-	std::vector<Book> books_from_file = readBooksFromIniFile(input_data);
-
-	// Print the data
+	std::vector<Book> books_from_file;
+	books_from_file = Book::readBooksFromIniFile(input_data);
 	std::cout << "Here are all the books found in the data file..." << std::endl;
 	for (auto book : books_from_file)
 	{
 		book.print();
 	}
-
 	return 0;
 }
--- 15_game_of_life/src/main.cpp
+++ 15_game_of_life/src/main.cpp
@@ -1,4 +1,288 @@
+//si aici am folosit fisiere header si cpp
+//GameOfLife.h:
+class GameOfLife
+{
+
+public:
+	void static init_grid();// facut
+	void  pick_Menu(); //facut
+	void print_grid();
+	void Block( int x, int y);
+	void Boat(int x , int y);
+	void Blinker(int x, int y);
+	void Beacon(int x, int y);
+	void Glider(int x, int y);
+	void DrawShape(int Shape, int x, int y);
+	bool checkLess2CellsAround(int xPunct, int yPunct);
+	bool check2or3Cells(int xPunct, int yPunct);
+	bool checkMore3Cells(int xPunct, int yPunct);
+	bool ResurectCell(int xPunct, int yPunct);
+	void Redraw(int movements);
+
+};
+//end of GameOfLife.h
+//GameOfLife.cpp
+//#include "GameOfLife.h"
+//#include <iostream>
+//#include<vector>
+
+
+
+const int rows = 20;
+const int cols = 20;
+int grid[rows][cols];
+int pick, xPunct, yPunct, movements;
+struct Point {
+	int x;
+	int y;
+	Point(int abs, int ord) {
+		this->x = abs;
+		this->y = ord;
+	}
+};
+
+enum Shape {
+	Block = 1,
+	Boat = 2,
+	Blinker = 3,
+	Beacon = 4,
+	Pulsar = 5,
+	Pentadecathlon = 6,
+	Glider = 7
+
+};
+
+void GameOfLife::init_grid() {
+	for (int x = 0; x < rows; x++) {
+		for (int y = 0; y < cols; y++) {
+			grid[x][y] = 0;
+		}
+	}
+};
+
+void GameOfLife::print_grid() {
+	for (int x = 0; x < rows; x++) {
+		for (int y = 0; y < cols; y++) {
+			std::cout << grid[x][y] << " ";
+
+		}
+		std::cout << std::endl;
+	}
+};
+
+void GameOfLife::pick_Menu() {
+	std::cout << "You can choose the form to display from this list: " << std::endl
+		<< "Option 1: Block" << std::endl
+		<< "Option 2: Boat" << std::endl
+		<< "Option 3: Blinker" << std::endl
+		<< "Option 4: Beacon " << std::endl
+		<< "Option 5: Glider " << std::endl
+		<< "Enter your option: ";
+	std::cin >> pick; //the option from user
+	std::cout << "Introduce the coordonates. Pick values <= 20." << std::endl;
+	do {
+		std::cout << "X: ";
+		std::cin >> xPunct;
+		std::cout << "Y: ";
+		std::cin >> yPunct;
+		if (xPunct >= 20 || yPunct >= 20)
+			std::cout << "Enter the coordinates again." << std::endl;
+	} while (xPunct >= 20 && yPunct >= 20);
+	std::cout << "Now enter how many movements:" << std::endl;
+	std::cin >> movements;
+	std::cout << "Show shape:" << std::endl;
+	GameOfLife::DrawShape(pick, xPunct, yPunct);
+}
+
+
+void GameOfLife::Block (int x, int y)
+{
+
+	grid[x - 1][y - 1] = 1;
+	grid[x][y - 1] = 1;
+	grid[x - 1][y] = 1;
+	grid[x][y] = 1;
+}
+
+void GameOfLife::Boat (int x, int y)
+{
+	grid[x - 1][y - 1] = 1;
+	grid[x][y - 1] = 1;
+	grid[x - 1][y] = 1;
+	grid[x][y + 1] = 1;
+	grid[x + 1][y] = 1;
+}
+
+void GameOfLife::Blinker(int x, int y)
+{
+	grid[x - 1][y - 1] = 1;
+	grid[x - 1][y - 2] = 1;
+	grid[x - 1][y ] = 1;
+}
+
+void GameOfLife::Beacon(int x, int y)
+
+{
+	//top left part
+	grid[x - 1][y - 1] = 1;
+	grid[x][y - 1] = 1;
+	grid[x - 1][y] = 1;
+	//bottom right part
+	grid[x + 2][y + 1] = 1;
+	grid[x + 1][y + 2] = 1;
+	grid[x + 2][y + 2] = 1;
+}
+
+void GameOfLife::Glider(int x, int y)
+{
+	grid[x - 2][y - 1] = 1;
+	grid[x - 1][y] = 1;
+	grid[x][y] = 1;
+	grid[x][y - 1] = 1;
+	grid[x][y - 2] = 1;
+}
+
+void GameOfLife::Redraw(int movements) {
+	for (int i = 0; i < movements; ++i) {
+		std::vector<Point> deaths;
+		std::vector<Point> alive;
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				if (grid[i][j] == 1) {
+					if (GameOfLife::checkLess2CellsAround(i, j) || GameOfLife::checkMore3Cells(i, j)) {
+						Point p(i, j);
+						deaths.push_back(p);
+					}
+					else if (GameOfLife::check2or3Cells(i, j)) {
+						Point p(i, j);
+						alive.push_back(p);
+					}
+				}
+				else if (grid[i][j] == 0) {
+					if (GameOfLife::ResurectCell(i, j)) {
+						Point p(i, j);
+						//cout << i << " " << j << endl;;
+						alive.push_back(p);
+					}
+				}
+
+		for (Point p : alive)
+			grid[p.x][p.y] = 1;
+		for (Point p : deaths)
+			grid[p.x][p.y] = 0;
+
+		std::cout << std::endl;
+		GameOfLife::print_grid();
+	}
+}
+
+bool GameOfLife::ResurectCell(int xPunct, int yPunct) {
+	int counter = 0;
+	for (int row = xPunct - 1; row < xPunct + 2 && row < rows; ++row)
+	{
+		for (int col = yPunct - 1; col < yPunct + 2 && col < cols; ++col)
+		{
+			if (grid[row][col])
+				counter++;
+		}
+	}
+
+	return counter == 3;
+}
+
+bool GameOfLife::checkLess2CellsAround(int xPunct, int yPunct) {
+	int count = 0;
+	for (int row = xPunct - 1; row < xPunct + 2 && row < rows; ++row)
+	{
+		for (int col = yPunct - 1; col < yPunct + 2 && col < cols; ++col)
+		{
+			if (grid[row][col])
+				count++;
+
+		}
+	}
+	return (count - 1) < 2;
+}
+
+bool GameOfLife::checkMore3Cells(int xPunct, int yPunct) {
+	int count = 0;
+	for (int row = xPunct - 1; row < xPunct + 2 && row < rows; ++row)
+	{
+		for (int col = yPunct - 1; col < yPunct + 2 && col < cols; ++col)
+		{
+			if (grid[row][col])
+				count++;
+
+		}
+	}
+	return (count - 1) > 3;
+}
+
+bool GameOfLife::check2or3Cells(int xPunct, int yPunct) {
+	int count = 0;
+	for (int row = xPunct - 1; row < xPunct + 2 && row < rows; ++row)
+	{
+		for (int col = yPunct - 1; col < yPunct + 2 && col < cols; ++col)
+		{
+			if (grid[row][col])
+				count++;
+
+		}
+	}
+	return count == 2 || count == 3;
+}
+
+void GameOfLife::DrawShape(int Shape, int Xcoord, int Ycoord) {
+
+	switch (Shape) {
+	case 1: {
+		GameOfLife::Block(Xcoord, Ycoord);
+		GameOfLife::print_grid();
+		GameOfLife::Redraw(movements);
+		break;
+	}
+	case 2: {
+		GameOfLife::Boat(Xcoord, Ycoord);
+		GameOfLife::print_grid();
+		GameOfLife::Redraw(movements);
+		break;
+	}
+	case 3: {
+		GameOfLife::Blinker(Xcoord, Ycoord);
+		GameOfLife::print_grid();
+		GameOfLife::Redraw(movements);
+		break;
+	}
+	case 4: {
+		GameOfLife::Beacon(Xcoord, Ycoord);
+		GameOfLife::print_grid();
+		GameOfLife::Redraw(movements);
+		break;
+	}
+
+	case 5: {
+		GameOfLife::Glider(Xcoord, Ycoord);
+		GameOfLife::print_grid();
+		GameOfLife::Redraw(movements);
+		break;
+	}
+	default: {
+		std::cout << "Wrong figure input.";
+		break;
+	}
+	}
+
+
+}
+//eof GameOfLife.cpp
+
+
 int main()
 {
+	std::cout << "Assigment 4 Game of Life on 20x20 grid" << std::endl;
+	GameOfLife var;
+	var.init_grid();
+	//var.print_grid();
+	var.pick_Menu();
 	return 0;
 }
